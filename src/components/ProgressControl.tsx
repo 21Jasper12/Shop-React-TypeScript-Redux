cbd85f66988e3022485bbf04e11c26b31784c697
21Jasper <pickeng2389002@gmail.com>
2023-02-05 16:46:28 +0800

    RWD完成

--- src/components/ProgressControl.tsx
+++ src/components/ProgressControl.tsx
@@ -13,7 +13,7 @@ const ProgressControl = ({ onNext }: props) => {
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
 
-  function handlePrevious():void {
+  function handlePrevious(): void {
     dispatch(ShoppingChangeStatus({ status: (statusNumber - 1) }))
   }
 
@@ -23,38 +23,78 @@ const ProgressControl = ({ onNext }: props) => {
     if ((statusNumber < 3) && nextStatus) {
       dispatch(ShoppingChangeStatus({ status: (statusNumber + 1) }))
     }
-    else if ((statusNumber === 3) && nextStatus && (totalCount !== 0)){
+    else if ((statusNumber === 3) && nextStatus && (totalCount !== 0)) {
       navigate('/orderInfo')
     }
-    else if ((statusNumber === 3) && (totalCount === 0)){
+    else if ((statusNumber === 3) && (totalCount === 0)) {
       alert('購物籃不得空白')
     }
   }
 
-  return(
-    <div className={styles.btn}>
-      {
-        statusNumber === 1 ? null : (<button className={styles.btnPrevious} onClick={handlePrevious}>← 上一步</button>)
-      }
-      {
-        statusNumber === 3 ? (
-          <button
-            className={styles.btnNext}
-            onClick={handleNext}
-          >
-            確認下單資料
-          </button>
-        ) :
-          (
+  return (
+    <div className={styles.btnModules}>
+      <div className={styles.computerBtn}>
+        {
+          statusNumber === 1 ? null : (<button className={styles.btnPrevious} onClick={handlePrevious}>← 上一步</button>)
+        }
+        {
+          statusNumber === 3 ? (
+            <button
+              className={styles.btnNext}
+              onClick={handleNext}
+            >
+              確認下單資料
+            </button>
+          ) :
+            (
+              <button
+                className={styles.btnNext}
+                onClick={handleNext}
+              >
+                下一步 →
+              </button>
+            )
+        }
+      </div>
+
+      {(statusNumber !== 1) ? (
+        <div className={styles.mobileBtn}>
+          {
+            statusNumber === 1 ? null : (<button className={styles.btnPrevious} onClick={handlePrevious}>← 上一步</button>)
+          }
+          {
+            statusNumber === 3 ? (
+              <button
+                className={styles.btnNext}
+                onClick={handleNext}
+              >
+                確認下單資料
+              </button>
+            ) :
+              (
+                <button
+                  className={styles.btnNext}
+                  onClick={handleNext}
+                >
+                  下一步 →
+                </button>
+              )
+          }
+        </div>
+      ) :
+      (
+        <div className={styles.mobileStepOneBtn}>
             <button
               className={styles.btnNext}
               onClick={handleNext}
             >
               下一步 →
             </button>
-          )
+        </div>
+      )
       }
     </div>
+
   )
 }
 
